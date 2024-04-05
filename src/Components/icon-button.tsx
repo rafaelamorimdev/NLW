0994a7fa760381a36242ae125069a61f47a98ca1
RafaelAmorim <rafael.ceb2010@gmail.com>
2024-04-05 16:02:28 -0300

    Ultima atualização

--- src/Components/icon-button.tsx
+++ src/Components/icon-button.tsx
@@ -1,17 +1,19 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-
+            // PRECISO COLOCAR A INTERFACE PARA FAZER O ...PROPS E TER A IMPORTAÇÃO
 interface iconButtonProps extends ComponentProps<'button'> {
     transparent? : boolean
 }
-
+           //CONDICIONAIS E ESTILIZAÇÃO DOS BOTÕES DE PASSAR A PÁGINA
 export function IconButton ({transparent, ...props}: iconButtonProps) {
     return (
         <button {...props}
-        className={
-            transparent 
-            ? " bg-black/20 border border-white/10 rounded-md p-1.5"
-            : "bg-white/10 border border-white/10 rounded-md p-1.5"}
+         
+            className={twMerge('bg-white/10 border border-white/10 rounded-md p-1.5',
+               transparent ? 'bg-black/20' : 'bg-white/10', 
+               props.disabled ? 'opacity-50' : null,
+            )}
         />
     )
 }
